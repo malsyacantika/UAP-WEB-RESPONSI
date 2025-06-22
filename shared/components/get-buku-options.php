8344a0e62d8e2ac49a0c69a2477cbae0f0b6305c
imamrifai8178 <151293714+imamrifai8178@users.noreply.github.com>
2025-06-22 23:48:55 +0700

    Delete shared/components/get-buku-options.php

--- shared/components/get-buku-options.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$query = $pdo->query("SELECT id, judul FROM data_buku ORDER BY judul");
-while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-    echo '<option value="' . $row['id'] . '">' . htmlspecialchars($row['judul']) . '</option>';
-}
